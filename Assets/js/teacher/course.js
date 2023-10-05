223e5e6c5598b33887f39801e1ad30eacb9046a4
benj-yalung <benjamin.yalung2020@gmail.com>
2023-10-05 15:41:55 +0800

    Change attendance chart location

--- Assets/js/teacher/course.js
+++ Assets/js/teacher/course.js
@@ -18,3 +18,31 @@ buttons.forEach((button, idx) => {
     );
   });
 });
+
+// Attendance Donut Chart
+const chart = document.getElementById("attendance-chart");
+
+new Chart(chart, {
+  type: "doughnut",
+
+  data: {
+    labels: ["On Time", "Late", "Absent"],
+    datasets: [
+      {
+        // label: "",
+        data: [9, 5, 1],
+        borderWidth: 1,
+        backgroundColor: ["#0F0346", "#28EDFC", "#A447FD"],
+        rotation: 135,
+      },
+    ],
+  },
+
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
